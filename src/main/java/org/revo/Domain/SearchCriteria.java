5ba6c2547438633550dfa30209517fc95005ebfb
ashraf <ashraf1abdelrasool@gmail.com>
2017-02-27 19:55:13 +0200

    setup searchly elasticsearch

--- src/main/java/org/revo/Domain/SearchCriteria.java
+++ src/main/java/org/revo/Domain/SearchCriteria.java
@@ -1,33 +1,18 @@
 package org.revo.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * Created by ashraf on 31/01/17.
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchCriteria {
     private String search;
     private Page page;
-
-    public SearchCriteria() {
-    }
-
-    public SearchCriteria(String search, Page page) {
-        this.search = search;
-        this.page = page;
-    }
-
-    public String getSearch() {
-        return search;
-    }
-
-    public void setSearch(String search) {
-        this.search = search;
-    }
-
-    public Page getPage() {
-        return page;
-    }
-
-    public void setPage(Page page) {
-        this.page = page;
-    }
 }
